e9502dfafd97082d6ac5b81be302214b7d31d6da
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-07-09 02:06:07 +0530

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,6 +1,5 @@
 def call() {
     stage('Static Analysis with SonarQube') {
-       withSonarQubeEnv('sonar') { // Replace 'SonarQubeServer' with your actual SonarQube server name
            sh 'mvn sonar:sonar'
     }
   }
